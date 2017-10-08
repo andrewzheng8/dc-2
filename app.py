2436457c9f388c027720905cb5055a7c6466017d
andrewzheng8 <andrew.zheng725@gmail.com>
2017-10-08 00:55:51 -0400

    added helper functions and keys

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import copy
 
 from boto.s3.connection import S3Connection
 
-# s3 = S3Connection(os.environ['GMAPS_KEY'], os.environ['GIPHY_KEY'])
+s3 = S3Connection(os.environ['GMAPS_KEY'], os.environ['GIPHY_KEY'])
 
 app = Flask(__name__)
 
@@ -28,11 +28,35 @@ def getPlacesAndGifs():
         if not isinstance(query, basestring):
             return 'query key in the request body json must point to a value of type string'
 
-        return 'Hello'
-        # buildJsonFile(query)
-        # return send_file('send.json', mimetype='application/json')
+        # return 'Hello'
+        buildJsonFile(query)
+        return send_file('send.json', mimetype='application/json')
 
     return 'Hello World! How are you even at this point\n'
+
+def getPlaces(query):
+    payload = {'key':s3['GMAPS_KEY'], 'query':query}
+    gmaps_response = requests.get(GMAPS_API, params=payload)
+    return gmaps_response.json()['results']
+
+def getGiphies(gmapsObj):
+    giphy_payload = {'api_key': s3['GIPHY_KEY'], 'q': gmapsObj['name']}
+    giphy_response = requests.get(GIPHY_API, params= giphy_payload)
+    giphy_results = giphy_response.json()['data']
+    return giphy_results
+
+def buildJsonObject(gmapsResults):
+    gmapsCopy = copy.deepcopy(gmapsResults)
+    for result in gmapsCopy:
+        result['giphies'] = getGiphies(result)
+    return gmapsCopy
+
+def buildJsonFile(query):
+    places = getPlaces(query)
+    withGiphies = buildJsonObject(places)
+    with open('send.json', 'w') as json_file:
+        json.dump(withGiphies, json_file)
+        json_file.close()
 
 
 if __name__ == '__main__':
